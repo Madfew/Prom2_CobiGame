da1916cfab8c93cdb269dfe7f1c9846dc78587b8
FrankyG2 <39641831+FrankyG2@users.noreply.github.com>
2018-06-02 12:08:43 -0500

    Scripts - 1ra versión
    El script del personaje cuenta con una variable de vida, los scripts del golpe y del veneno acceden a esta variable y la modifican. El golpe hace 20 de daño con cada click mientras que el veneno hace 5 de daño constante por 5 segundos. Aún no lo he probado pero es la idea con la que trabajaremos.

---
+++ Golpe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golpe : MonoBehaviour {
+
+	public GameObject personaje;
+	int	vidaPersonaje;
+
+	void Start () {
+		
+		vidaPersonaje = personaje.GetComponent<Personaje> ().vida;
+			
+	}
+
+	void OnClick() {
+
+		vidaPersonaje = vidaPersonaje - 20;
+	}
+}
