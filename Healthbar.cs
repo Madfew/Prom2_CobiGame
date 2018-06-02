acea2ebcac48c48f11dc3bd3287f33a842a1b6d5
Chumoi <fernandochumpitaz02@gmail.com>
2018-06-02 12:34:34 -0500

    Healthbar script añadido

---
+++ Healthbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour {
+
+    Image Healthbar
+    float maxHealth = 100f;
+    public static float health;
+
+    // Use this for initialization
+    void Start() {
+        healthbar = GetComponent<Image>();
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Healthbar.fillAmount = health / maxHealth;
+	}
+}
